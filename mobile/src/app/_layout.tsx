1849390d4c4fe61fab48434e609f6d5d988af94b
Rondnelymachado-maker <rondnely.machado@icloud.com>
2026-09-24 09:00:34 -0300

    fix(mobile): remove status bar dependency from root layout

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#172033" />
-      <Stack screenOptions={{ headerShown: false }} />
-    </>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
